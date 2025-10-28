429c43005629adf76a0712bee6dde5e77feceec2
superskibidi <rookiecookierc@gmail.com>
2025-10-28 16:32:50 +0000

    add download vbuttons for ios

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function previewEncodeImage() {
 
   $(".images .nulled").hide();
   $(".images .message").hide();
+  // Disable download until a new encoded image is created
+  $(".download").prop('disabled', true);
 
   previewImage(file, ".original canvas", function() {
     $(".images .original").fadeIn();
@@ -98,7 +100,7 @@ function encodeMessage() {
 
   // Convert the message to a binary string
   var binaryMessage = "";
-  for (i = 0; i < text.length; i++) {
+  for (var i = 0; i < text.length; i++) {
     var binaryChar = text[i].charCodeAt(0).toString(2);
 
     // Pad with 0 until the binaryChar has a lenght of 8 (1 Byte)
@@ -113,7 +115,7 @@ function encodeMessage() {
   // Apply the binary string to the image and draw it
   var message = nulledContext.getImageData(0, 0, width, height);
   pixel = message.data;
-  counter = 0;
+  var counter = 0;
   for (var i = 0, n = pixel.length; i < n; i += 4) {
     for (var offset =0; offset < 3; offset ++) {
       if (counter < binaryMessage.length) {
@@ -130,13 +132,55 @@ function encodeMessage() {
   $(".binary").fadeIn();
   $(".images .nulled").fadeIn();
   $(".images .message").fadeIn();
+  // enable the download button now that the message canvas exists
+  $(".download").prop('disabled', false);
 };
+
+// Download the message canvas as an image (PNG)
+function downloadMessageImage() {
+  var canvas = document.querySelector('.message canvas');
+
+  if (!canvas) {
+    alert('No encoded message canvas available to download.');
+    return;
+  }
+
+  // Prefer toBlob when available for binary download and better memory usage
+  if (canvas.toBlob) {
+    canvas.toBlob(function(blob) {
+      var url = URL.createObjectURL(blob);
+      var a = document.createElement('a');
+      a.style.display = 'none';
+      a.href = url;
+      a.download = 'stego.png';
+      document.body.appendChild(a);
+      a.click();
+      // cleanup
+      setTimeout(function() {
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+      }, 100);
+    }, 'image/png');
+  }
+  else {
+    // Fallback for older browsers
+    var dataURL = canvas.toDataURL('image/png');
+    var a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = dataURL;
+    a.download = 'stego.png';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+}
 
 function decodeMessage() {
   var $originalCanvas = $('.decode canvas');
   var originalContext = $originalCanvas[0].getContext("2d");
 
-  var original = originalContext.getImageData(0, 0, $originalCanvas.width(), $originalCanvas.height());
+  // Use the actual canvas pixel dimensions (DOM properties) when reading pixels
+  var original = originalContext.getImageData(0, 0, $originalCanvas[0].width, $originalCanvas[0].height);
   var binaryMessage = "";
   var pixel = original.data;
   for (var i = 0, n = pixel.length; i < n; i += 4) {
